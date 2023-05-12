3b409c672ec55eadaebe5e37f505c2dc15102a80
Chris <chriscablish@hotmail.com>
2023-05-12 14:54:48 -0500

    add comments for clarity

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//variables
 const birthdayForm = document.getElementById('birthday__form');
 const dayEntered = document.getElementById('input__day');
 const monthEntered = document.getElementById('input__month');
@@ -10,13 +10,11 @@ const currentYear = new Date().getFullYear();
 const dayErrorMessage = document.getElementById('day__error');
 const monthErrorMessage = document.getElementById('month__error');
 const yearErrorMessage = document.getElementById('year__error');
-
-
 const yearsDisplay = document.getElementById('years__number');
 const monthsDisplay = document.getElementById('months__number');
 const daysDisplay = document.getElementById('days__number');
 
-
+// display "--" at start and if date is invalid;
 const setValuesToNull = () => {
     yearsDisplay.innerText = '--';
     monthsDisplay.innerText = '--';
@@ -25,11 +23,11 @@ const setValuesToNull = () => {
 
 setValuesToNull();
 
-
-
+//click event for submit
 birthdayForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
+    //find difference between current d/m/y and birth d/m/y
     const calculateDays = () => {
         return currentDay - parseInt(dayEntered.value);
     }
@@ -42,41 +40,51 @@ birthdayForm.addEventListener('submit', (event) => {
         return currentYear - parseInt(yearEntered.value);
         }
 
+    //store differences in var
     let dayDifference = calculateDays();
     let monthDifference = calculateMonths();
     let yearDifference = calculateYears();
 
+    //if the difference in days is less than 0, subtract the absolute value of dayDifference from the total number of days in that month. 
     if (dayDifference < 0) {
         dayDifference = numberOfDays + dayDifference;
+        //negative days means we "go back" one month
         monthDifference = monthDifference -1;
     }
 
+    //if the difference in months is less than 0, subtract the absolute value of monthDifference from 12 and subtract 1 from the year. 
     if (monthDifference < 0) {
         monthDifference = 12 + monthDifference;
         yearDifference = yearDifference -1;
     }
 
+    //if yearDiffernce is less than 0 it means user has input a future year, which is invalid
     if (yearDifference < 0) {
         yearErrorMessage.style.display = 'inline';
         setValuesToNull();
     }
 
+    //update diplays to show elapsed time between current date and birthdate (aka users age)
     yearsDisplay.innerText = yearDifference;
     monthsDisplay.innerText = monthDifference;
     daysDisplay.innerText = dayDifference; 
 
-
+    //store return value of checkDays()
     const isARealDay = checkDays(parseInt(dayEntered.value), parseInt(monthEntered.value));   
 
+    //check if the month entered by user is outside range of 1-12
     if (parseInt(monthEntered.value) > 12 || parseInt(monthEntered.value) < 1) {
+        //if it is out of range, display error and do not display age
         monthErrorMessage.style.display = 'inline';
         setValuesToNull();
+        //also if month is out of range, check if day value is within accapetable day value parameters (number between 1 and 31)
         if (parseInt(dayEntered.value) > 31 || parseInt(dayEntered.value) < 1) {
+            //if day is out of range, display day error message
             dayErrorMessage.style.display = 'inline';
         }
         return;
     }
-
+    //check if day is valid. 
     if (isARealDay === false) {
         dayErrorMessage.style.display = 'inline';
         setValuesToNull();
@@ -84,7 +92,7 @@ birthdayForm.addEventListener('submit', (event) => {
 
 });
 
-
+//a function to check whether D/M combo is valid
 const checkDays = (day, month) => {
     if (
       (month === 1 ||
@@ -109,7 +117,7 @@ const checkDays = (day, month) => {
     }  
   };
   
-
+//a function to determine the number of days in each month
 const findNumberOfDays = (month) => {
     if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
         return 31;
